1396f89c21c8e1817c51f8aa8257841165a4f893
chaevid <chaevid@gmail.com>
2023-05-11 19:23:32 +0900

    2.3 Responses

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ import express from 'express';
 const app = express();
 const PORT = 4000;
 
-const handleHome = () => console.log('Somebody is trying to go home.');
+const handleHome = (req, res) => {
+  return res.send('<h1>I still love you.</h1>');
+};
+
+const handleLogin = (req, res) => {
+  return res.json({ msg: 'Login here.' });
+};
 
 app.get('/', handleHome);
+app.get('/login', handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on: http://localhost:${PORT} 🚀`);
