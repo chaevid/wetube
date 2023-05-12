41864d24bb2c9305cbb11de198ad5a860d8ce7c6
chaevid <chaevid@gmail.com>
2023-05-12 13:33:59 +0900

    2.10 Middleware Recap

--- src/server.js
+++ src/server.js
@@ -3,38 +3,28 @@ import express from 'express';
 const app = express();
 const PORT = 4000;
 
-const logger = (req, res, next) => {
-  console.log(`Someone is going to '${req.url}' with '${req.method}' method`);
+const methodLogger = (req, res, next) => {
+  console.log(`Method : '${req.method}'`);
   next();
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-
-  if (url === '/protected') {
-    return res.send('<h1>Not Allowed</h1>');
-  }
-  console.log('Allowed, you may continue.');
+const routerLogger = (req, res, next) => {
+  console.log(`Path : '${req.path}'`);
   next();
 };
 
-const handleHome = (req, res) => {
-  return res.send('<h1>I still love you.</h1>');
+const home = (req, res) => {
+  return res.send('<h1>Home</h1>');
 };
 
-const handleLogin = (req, res) => {
+const login = (req, res) => {
   return res.json({ msg: 'Login here.' });
 };
 
-const handleProtected = (req, res) => {
-  return res.send('<h1>Welcome to the private lounge.</h1>');
-};
+app.use(methodLogger, routerLogger);
 
-app.use(logger); // Must be before app.get()
-app.use(privateMiddleware);
-app.get('/', handleHome);
-app.get('/login', handleLogin);
-app.get('/protected', handleProtected);
+app.get('/', home);
+app.get('/login', login);
 
 const handleListening = () =>
   console.log(`✅ Server listening on: http://localhost:${PORT} 🚀`);
