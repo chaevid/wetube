f17a10bb859ad59453ef4153bfb77318ef403f31
chaevid <chaevid@gmail.com>
2023-05-15 16:56:34 +0900

    4.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
-export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home' }); // Variable
 export const search = (req, res) => res.send('Search Video');
 export const see = (req, res) =>
-  res.render('watch', { pageTitle: 'Watch', videoId: req.params.id });
+  res.render('watch', { pageTitle: 'Watch', videoId: req.params.id }); // Variable
 export const edit = (req, res) =>
-  res.render('edit', { pageTitle: 'Watch', videoId: req.params.id });
+  res.render('edit', { pageTitle: 'Watch', videoId: req.params.id }); // Variable
 export const deleteVideo = (req, res) => res.send('Delete Video');
 export const upload = (req, res) => res.send('Upload Video');
