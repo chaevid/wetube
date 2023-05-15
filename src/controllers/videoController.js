0debbf7c8aec9e725119755aa4ed931b5bc9f07a
chaevid <chaevid@gmail.com>
2023-05-15 17:26:58 +0900

    4.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,14 @@
-export const trending = (req, res) => res.render('home', { pageTitle: 'Home' }); // Variable
+const fakeUser = {
+  username: 'chaevid',
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render('home', { pageTitle: 'Home', fakeUser }); // ES6
 export const search = (req, res) => res.send('Search Video');
 export const see = (req, res) =>
-  res.render('watch', { pageTitle: 'Watch', videoId: req.params.id }); // Variable
+  res.render('watch', { pageTitle: 'Watch', videoId: req.params.id });
 export const edit = (req, res) =>
-  res.render('edit', { pageTitle: 'Watch', videoId: req.params.id }); // Variable
+  res.render('edit', { pageTitle: 'Watch', videoId: req.params.id });
 export const deleteVideo = (req, res) => res.send('Delete Video');
 export const upload = (req, res) => res.send('Upload Video');
