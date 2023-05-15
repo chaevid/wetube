206696b800365a7ab40a966db72a1a69b281c5da
chaevid <chaevid@gmail.com>
2023-05-15 12:34:46 +0900

    3.8 URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,9 +4,9 @@ import { see, edit, deleteVideo, upload } from '../controllers/videoController';
 const videoRouter = express.Router();
 
 videoRouter.get('/upload', upload);
-videoRouter.get('/:id', see);
-videoRouter.get('/:id/edit', edit);
-videoRouter.get('/:id/delete', deleteVideo);
+videoRouter.get('/:id(\\d+)', see);
+videoRouter.get('/:id(\\d+)/edit', edit);
+videoRouter.get('/:id(\\d+)/delete', deleteVideo);
 // videoRouter.get('/upload', upload); <<<<<<< 이렇게 하면 /upload가 /:id로 인식되어버림
 
 export default videoRouter;
