c57c57485ae1077db552f76eea749a8613cab140
chaevid <chaevid@gmail.com>
2023-05-15 16:20:07 +0900

    4.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,7 @@
 export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
 export const search = (req, res) => res.send('Search Video');
-export const see = (req, res) => res.send(`See Video #${req.params.id}`);
-export const edit = (req, res) => res.send('Edit Video');
+export const see = (req, res) =>
+  res.render('watch', { pageTitle: 'Watch', videoId: req.params.id });
+export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
 export const deleteVideo = (req, res) => res.send('Delete Video');
 export const upload = (req, res) => res.send('Upload Video');
