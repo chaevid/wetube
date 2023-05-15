ad90bfb59e996136ed89060e9877ea265ab2ff91
chaevid <chaevid@gmail.com>
2023-05-15 16:44:43 +0900

    4.3 Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,7 @@ export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
 export const search = (req, res) => res.send('Search Video');
 export const see = (req, res) =>
   res.render('watch', { pageTitle: 'Watch', videoId: req.params.id });
-export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
+export const edit = (req, res) =>
+  res.render('edit', { pageTitle: 'Watch', videoId: req.params.id });
 export const deleteVideo = (req, res) => res.send('Delete Video');
 export const upload = (req, res) => res.send('Upload Video');
